b532a9431c28d0a47437c3a3cc02b69973a1444b
nicolasmichielon <nicolasmichielon@gmail.com>
2025-01-17 12:05:09 -0300

    Add type declarations for PNG and SVG files; create theme components

--- App.tsx
+++ App.tsx
@@ -1,28 +1,27 @@
-import { Text, View, StatusBar } from "react-native";
+import { StatusBar } from "react-native";
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
+import { GluestackUIProvider, Text, Center } from "@gluestack-ui/themed";
+import { config } from "./config/gluestack-ui.config";
+
+import { Loading } from "@components/Loading";
+import { SignIn } from "@screens/SignIn";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#202024",
-      }}
-    >
+    <GluestackUIProvider config={config}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Text>Home</Text> : <View></View>}
-    </View>
+
+      {fontsLoaded ? <SignIn /> : <Loading />}
+    </GluestackUIProvider>
   );
 }
---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,25 @@
+import { VStack, Image, Center, Text } from "@gluestack-ui/themed";
+
+import BackgroundImg from "@assets/background.png";
+import Logo from "@assets/logo.svg";
+
+export function SignIn() {
+  return (
+    <VStack flex={1} bg="$gray700">
+      <Image
+        w="$full"
+        height={624}
+        source={BackgroundImg}
+        defaultSource={BackgroundImg}
+        alt="Pessoas treinando"
+        position="absolute"
+      />
+      <Center my="$24">
+        <Logo />
+        <Text color="$gray100" fontSize="$sm">
+          Treine sua mente e o seu corpo.
+        </Text>
+      </Center>
+    </VStack>
+  );
+}
