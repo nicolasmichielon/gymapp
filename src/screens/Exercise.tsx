dea5396220091c47e5e21c7040dc1226c3cc0463
nicolasmichielon <nicolasmichielon@gmail.com>
2025-01-20 12:46:09 -0300

    Refactor Exercise and Home screens to enhance navigation; update HistoryCard layout and add exercise details display

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,9 +1,103 @@
-import { Center, Text } from "@gluestack-ui/themed";
+import { ScrollView, TouchableOpacity } from "react-native";
+import {
+  VStack,
+  Icon,
+  HStack,
+  Heading,
+  Text,
+  Image,
+  Box,
+} from "@gluestack-ui/themed";
+import { ArrowLeft } from "lucide-react-native";
+import { useNavigation } from "@react-navigation/native";
+
+import { Button } from "@components/Button";
+
+import { AppNavigatorRoutesProps } from "@routes/app.routes";
+
+import BodySvg from "@assets/body.svg";
+import SeriesSvg from "@assets/series.svg";
+import RepetitionsSvg from "@assets/repetitions.svg";
 
 export function Exercise() {
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
   return (
-    <Center flex={1}>
-      <Text color="$white">Exercise</Text>
-    </Center>
+    <VStack flex={1}>
+      <VStack px="$8" bg="$gray600" pt="$12">
+        <TouchableOpacity onPress={handleGoBack}>
+          <Icon as={ArrowLeft} color="$green500" size="xl" />
+        </TouchableOpacity>
+        <HStack
+          justifyContent="space-between"
+          alignItems="center"
+          mt="$4"
+          mb="$8"
+        >
+          <Heading
+            color="$gray100"
+            fontFamily="$heading"
+            fontSize="$lg"
+            flexShrink={1}
+          >
+            Puxada frontal
+          </Heading>
+          <HStack alignItems="center">
+            <BodySvg />
+
+            <Text color="$gray200" ml="$1" textTransform="capitalize">
+              Costas
+            </Text>
+          </HStack>
+        </HStack>
+      </VStack>
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 32 }}
+      >
+        <VStack p="$8">
+          <Image
+            source={{
+              uri: "https://i0.wp.com/shapelendario.com.br/wp-content/uploads/2024/03/face-pull-treino-de-bracos-shape-lendario.jpg?resize=474%2C474&ssl=1",
+            }}
+            alt="Exercício"
+            mb="$3"
+            resizeMode="cover"
+            rounded="$lg"
+            w="$full"
+            h="$80"
+          />
+          <Box bg="$gray600" rounded="$md" pb="$4" px="$4">
+            <HStack
+              alignItems="center"
+              justifyContent="space-around"
+              mb="$6"
+              mt="$5"
+            >
+              <HStack>
+                <SeriesSvg />
+                <Text color="$gray200" ml="$2">
+                  3 séries
+                </Text>
+              </HStack>
+
+              <HStack>
+                <RepetitionsSvg />
+                <Text color="$gray200" ml="$2">
+                  12 repetições
+                </Text>
+              </HStack>
+            </HStack>
+
+            <Button title="Marcar como realizado" />
+          </Box>
+        </VStack>
+      </ScrollView>
+    </VStack>
   );
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
 
 import { FlatList } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 import { VStack, HStack, Heading, Text } from "@gluestack-ui/themed";
+
+import { AppNavigatorRoutesProps } from "@routes/app.routes";
 
 import { HomeHeader } from "@components/HomeHeader";
 import { Group } from "@components/Group";
@@ -21,6 +24,12 @@ export function Home() {
     "Ombro",
   ]);
   const [groupSelected, setGroupSelected] = useState("Costas");
+
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+  function handleOpenExerciseDetails() {
+    navigation.navigate("exercise");
+  }
 
   return (
     <VStack flex={1}>
@@ -56,7 +65,9 @@ export function Home() {
         <FlatList
           data={exercises}
           keyExtractor={(item) => item}
-          renderItem={() => <ExerciseCard />}
+          renderItem={() => (
+            <ExerciseCard onPress={handleOpenExerciseDetails} />
+          )}
           showsVerticalScrollIndicator={false}
           contentContainerStyle={{ paddingBottom: 20 }}
         />
