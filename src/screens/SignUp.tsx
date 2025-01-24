e2a5a0edb55df3eb8867b32970761f136bac7da7
nicolasmichielon <nicolasmichielon@gmail.com>
2025-01-24 16:20:01 -0300

    Add axios for API requests; implement error handling in SignUp screen with toast notifications

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -6,16 +6,22 @@ import {
   Text,
   Heading,
   ScrollView,
+  useToast,
 } from "@gluestack-ui/themed";
 import { useForm, Controller } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 
+import { api } from "@services/api";
+
 import BackgroundImg from "@assets/background.png";
 import Logo from "@assets/logo.svg";
 
+import { AppError } from "@utils/AppError";
+
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+import { ToastMessage } from "@components/ToastMessage";
 
 type FormDataProps = {
   name: string;
@@ -40,6 +46,8 @@ const signUpSchema = yup.object({
 export function SignUp() {
   const navigation = useNavigation();
 
+  const toast = useToast();
+
   const {
     control,
     handleSubmit,
@@ -52,13 +60,28 @@ export function SignUp() {
     navigation.goBack();
   }
 
-  function handleSignUp({
-    name,
-    email,
-    password,
-    password_confirm,
-  }: FormDataProps) {
-    console.log({ name, email, password, password_confirm });
+  async function handleSignUp({ name, email, password }: FormDataProps) {
+    try {
+      const response = await api.post("/users", { name, email, password });
+      console.log(response.data);
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError
+        ? error.message
+        : "Não foi possível criar a conta. Tente novamente mais tarde.";
+
+      toast.show({
+        placement: "top",
+        render: ({ id }) => (
+          <ToastMessage
+            id={id}
+            action="error"
+            title={title}
+            onClose={() => toast.close(id)}
+          />
+        ),
+      });
+    }
   }
 
   return (
